f7593fff77c0d0f83e89f3654ac6b9f545d2b945
Gupta, Sunny <sunnyvales789@gmail.com>
2024-05-11 13:46:52 +0530

    update

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -6,55 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic.component.scss']
 })
 export class BasicComponent {
-  usaCountries = ['Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    ' New Mexico',
-    'New York',
-    'North Carolina',
-    ' North Dakota',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'West Virginia ',
-    'Wisconsin',
-    'Wyoming'];
 
   europeCountries = [
     'Albania',
@@ -72,14 +23,14 @@ export class BasicComponent {
     'Denmark',
     'Estonia',
     'Finland',
-    'France- Corsica-island',
+    'France',
     'Georgia',
     'Germany',
     'Greece',
     'Hungary',
     'Iceland',
-    'Ireland- Dublin- largest city and capital of Ireland , Belfast is the capital and largest city of Northern Ireland',
-    'Italy- Venice-capital city-Veneto-province-Italy - Italy is divided into 20 provinces (states)-',
+    'Ireland',
+    'Italy',
     'Kazakhstan',
     'Kosovo',
     'Latvia',
@@ -101,9 +52,9 @@ export class BasicComponent {
     'Serbia',
     'Slovakia',
     'Slovenia',
-    'Spain - its autonomous communities are divided into fifty provinces- Barcelona, Madrid',
+    'Spain',
     'Sweden',
-    'Switzerland- geneva-city-Switzerland-country',
+    'Switzerland',
     'Turkey',
     'Ukraine',
     'United Kingdom (UK or Britain or England)',
@@ -157,7 +108,7 @@ export class BasicComponent {
     'Timor-Leste',
     'Turkey',
     'Turkmenistan',
-    '  United Arab Emirates (UAE)',
+    'United Arab Emirates (UAE)',
     'Uzbekistan',
     'Vietnam',
     'Yemen'
@@ -241,7 +192,7 @@ export class BasicComponent {
     'panama',
     'st.kittis and Nevis',
     'st.lucia',
-    ' st.vincent and the Grenadines',
+    'st.vincent and the Grenadines',
     'Trinidad and Tobago',
     'usa'
   ];
@@ -261,8 +212,6 @@ export class BasicComponent {
     'venezuela'
   ];
 
-
-
   ausStates = [
     'New South Wales',
     ' Queensland',
@@ -275,188 +224,10 @@ export class BasicComponent {
     'Northern Territory',
   ];
 
-  germnyStates = [
-    'Baden-Württemberg',
-    'Bavaria',
-    'Berlin',
-    'Brandenburg',
-    'Bremen',
-    'Hamburg',
-    'Hesse',
-    'Lower Saxony',
-    'Mecklenburg-Vorpommern',
-    'North Rhine-Westphalia',
-    'Rhineland-Palatinate',
-    'Saarland',
-    'Saxony',
-    'Saxony-Anhalt',
-    'Schleswig-Holstein',
-    'Thuringia'
-  ];
-
-  netherlandpro = [
-    'North Holland',
-    'Drenthe',
-    'Flevoland',
-    'Friesland',
-    'Gelderland',
-    'Groningen',
-    'Limburg',
-    'North Brabant',
-    'North Holland',
-    'Overijssel',
-    'South Holland',
-    'Utrecht',
-  ];
-
 }
 
-// India is divided into six regions-
-// Central India	2	-              (Chhattisgarh and Madhya Pradesh)
-// East India	4	1              (Bihar, Jharkhand, West Bengal, Odisha)
-// North India	7	2              (UP, Haryana, Himachal Pradesh, Jammu and Kashmir, Punjab, Rajasthan, Uttrakhand)
-// Northeast India 8	-              (Seven Sister States (Arunachal Pradesh, Assam, Manipur, Meghalaya, Mizoram, Nagaland, and Tripura), and the Himalayan state of .Sikkim)
-// South India	5	2              (Andhra Pradesh, Karnataka, Kerala, Tamil Nadu and Telangana)
-// Western India   3       2              (Goa, Gujarat, and Maharashtra)
-
-// There is 75 district in up.
-
-// city                         state
-// Ajmer                         rajasthan
-// Ayodhya               	      U.P
-// Azamgarh          	      U.P
-// Banglore(capital)     	      Karnataka 	ISRO- Banglore (Banglore is the silicon valley of India).
-
-// BHUBNESHWAR                   Odhisa
-// Budaun                        U.P
-// chennai(madras)(capital)      Tamilnadu
-// coimbatore                    Tamilnadu
-// Darjeling                     westbengal
-// etawah                        up
-// ghazipur                      up
-// Guwhati(largest)              Assam
-// HARIDWAR                      uttrakhand
-// Hyderabad                     Telangana
-// Indore                        madhyapradesh
-// jaipur(capital)               rajasthan
-// jalandhar                     punjab
-// jhansi                        up
-// kharagpur                     westbengal
-// kolhapur                      maharashtra
-// kolkata(calcutta)(capital)    westbengal
-// kota                          Rajasthan
-// Manipal                       Karnataka
-// Manglore(port city)           Karnataka
-// Muzaffarnagar(delhi ncr)      Up
-// ooty(hillstation)             tamilnadu
-// panchkula                     haryana
-// pune                          maharashtra
-// Raipur(capital)               chattishgarh
-// rohtak                        haryana
-// sonipat                       haryana
-// srinagar-indian administered state-j&k
-// surat                         gujrat
-// udaipur                       Rajasthan
-// vijayawada                    andhrapradesh
-// Visakhapatnam                 andhrapradesh
 
 
 
 
-// North-Holland-state-Amsterdam-capital city-Netherland-country
-// holland or Netherland-12 province
-// 1.	Drenthe
-// 2.	Flevoland
-// 3.	Friesland
-// 4.	Gelderland
-// 5.	Groningen
-// 6.	Limburg
-// 7.	North Brabant
-// 8.	North-Holland
-// 9.	Overijssel
-// 10.	South Holland
-// 11.	Utrecht
-// 12.	Zeeland
-
-
-// Beijing-capital-china
-// Hongkong-special administrative system-china
-// Tibet-autonomous region -china
-
-/*----America----*/
-// boston-city-massachussetes-state-usa
-// silicon valley- southern portion-san fransico-city-california(CA)-usa
-// san diego-city-california-state-usa
-// sunnyvale-city-california-state-usa
-// san francisco-hilly city-northern california-state-USA
-// los angeles-city-california-state-usa
-// newyork state-usa
-// denver city-colorado state-usa
-// columbus city-ohio state-usa
-// Anchorage city-alaska state-usa
-// Lansing city-michigan  state-usa
-// Honolulu city-hawaii state-usa
-// Miami city-florida state-usa
-// Seattle city-washington state-usa (NASA- Washington D.C)
-// Salt Lake City-utah state-usa
-// Des Moines City-iowa state-usa
-// Lincoln City-nebraska state-usa
-// birmingham City-alabama state-usa
-// Las Vegas City-nevada state-usa
-// area51-airbase-nevada-state-usa
-// Tulsa City-oklahoma state-usa
-// Wichita City-kansas state-usa
-// Portland City-oregon state-usa
-// Montpelier City-vermont state-usa
-// houston City-texas state-usa
-// phoenix City-arizona state-usa
-// baltimore-city-Maryland-state-usa
-
-// Cambridge-city-England (UK)
-// (UK or Britain or England)-
-// London is the capital and largest city of England and the United Kingdom.
-// Scotland is a part of the UK and its capital is Edinburgh.
-// york is a walled city in northeast England.
-// England is divided into 9 governmental regions-
-// 1.	South West
-// 2.	South East
-// 3.	London
-// 4.	East of England
-// 5.	Yorkshire and the Humber
-// 6.	West Midlands
-// 7.	East Midlands
-// 8.	North West
-// 9.	North East
-
-// Auckland-city-Newzealand
-
-// Australia -6 federal states
-// New South Wales
-// Queensland
-// South Australia
-// Tasmania
-// Victoria
-// Western Australia
-// -3 territories
-// Australian Capital Territory
-// Jervis Bay Territory
-// Northern Territory
-
-// Germany-16 states
-// Baden-Württemberg
-// Bavaria
-// Berlin
-// Brandenburg
-// Bremen
-// Hamburg
-// Hesse
-// Lower Saxony
-// Mecklenburg-Vorpommern
-// North Rhine-Westphalia
-// Rhineland-Palatinate
-// Saarland
-// Saxony
-// Saxony-Anhalt
-// Schleswig-Holstein
-// Thuringia
 
