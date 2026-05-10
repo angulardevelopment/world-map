5bc9895338ab043562b4147c5dc59044d8d56afc
sunny7899 <sunnyvales789@gmail.com>
2026-05-10 12:06:58 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BasicComponent } from './basic/basic.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [BasicComponent]
 })
 export class AppComponent {
 }
--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
-  styleUrls: ['./basic.component.scss']
+  styleUrls: ['./basic.component.scss'],
+  standalone: true,
 })
 export class BasicComponent {
 
